7f3c91526e25256b1f0bf92fa49cb827c0bd1490
nizsheanez <www.pismeco@gmail.com>
2012-07-24 22:35:09 +0400

    news more

--- www/protected/modules/news/views/news/view.php
+++ www/protected/modules/news/views/news/view.php
@@ -1,17 +1,12 @@
-<?php if (isset($thumb)): ?>
-    <?php echo $thumb; ?>
-    <br/>
-    <br/>
-<?php endif ?>
+<?php $this->page_title = 'Наши <span>Работы</span>'; ?>
+<div class="news-detail">
+    <h3><?php echo $model->title . ' | ' . date('d.m.Y', strtotime($model->date)) ?></h3>
+    <?php echo $model->content; ?>
 
-<?php echo $model->content; ?>
+    <br clear='all'/>
 
-<br clear='all' />
-
-<?php $this->widget('fileManager.portlets.FileList', array(
+    <?php $this->widget('fileManager.portlets.FileList', array(
     'model' => $model,
-    'tag' => 'files'
+    'tag'   => 'files'
 )) ?>
-
-
-
+</div>
