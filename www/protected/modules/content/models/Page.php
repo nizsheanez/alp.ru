7d6a6a9d8d34f5d281a8b9a8f2d8901fb26deb2d
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-08-01 20:39:33 +0400

    add sidebar_top

--- www/protected/modules/content/models/Page.php
+++ www/protected/modules/content/models/Page.php
@@ -49,17 +49,15 @@ class Page extends ActiveRecordModel
             ), array(
                 'title', 'length',
                 'max'=> 200
-            ), array('text', 'safe'), array('meta_tags, god', 'safe'), array(
+            ), array('text, sidebar_top', 'safe'), array('meta_tags, god', 'safe'), array(
                 'title, url', 'filter',
                 'filter' => 'strip_tags'
             ), array(
-                'back_left', 'file',
                 'types'      => 'jpg, jpeg, gif, png, tif',
                 'allowEmpty' => true,
                 'maxSize'    => 1024 * 1024 * 2.5,
                 'tooLarge'   => 'Максимальный размер файла 2.5 Мб'
             ), array(
-                'back_center', 'file',
                 'types'      => 'jpg, jpeg, gif, png, tif',
                 'allowEmpty' => true,
                 'maxSize'    => 1024 * 1024 * 2.5,
@@ -134,8 +132,6 @@ class Page extends ActiveRecordModel
     public function uploadFiles()
     {
         return array(
-            'back_left'   => array('dir' => self::UPLOAD_PATH),
-            'back_center' => array('dir' => self::UPLOAD_PATH)
         );
     }
 
@@ -153,31 +149,5 @@ class Page extends ActiveRecordModel
     }
 
 
-    public function getBackLeft()
-    {
-        if ($this->back_left)
-        {
-            $img = '/' . self::UPLOAD_PATH . '/' . $this->back_left;
-        }
-        else
-        {
-            $img = '/images/left.jpg';
-        }
-        return "style='background: url({$img}) no-repeat'";
-    }
-
-
-    public function getBackCenter()
-    {
-        if ($this->back_center)
-        {
-            $img = '/' . self::UPLOAD_PATH . '/' . $this->back_center;
-            return "style='background: url({$img}) no-repeat'";
-        }
-        else
-        {
-            return '';
-        }
-    }
 }
 
