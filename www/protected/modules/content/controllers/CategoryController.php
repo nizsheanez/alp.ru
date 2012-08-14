5f8f9d642eaee46e81fd6dea47ece72e746f7343
Alexey Sharov <alexey.sharov@mebelrama.ru>
2012-08-14 18:00:04 +0400

    last

--- www/protected/modules/content/controllers/CategoryController.php
+++ www/protected/modules/content/controllers/CategoryController.php
@@ -20,6 +20,7 @@ class CategoryController extends BaseController
         $this->setMetaTags($page);
         $this->sidebar_top = $page->sidebar_top;
         $this->sidebar_top_title = $page->sidebar_top_title;
+        $this->cur_link = 'service';
 
         $this->render('service', array('html' => Category::getHtmlTree()));
     }
@@ -34,7 +35,7 @@ class CategoryController extends BaseController
         $this->sidebar_top_title = $page->sidebar_top_title;
 
 
-        $this->cur_link = 'prices';
+        $this->cur_link = 'price';
         $this->render('prices', array('html' => Category::getHtmlTree(false, 1), 'page' => $page));
     }
 
@@ -43,7 +44,7 @@ class CategoryController extends BaseController
     {
         $model = $category = $this->loadModel($alias ? $alias : $id, array('published'), $alias ? 'alias' : null);
 
-        $this->cur_link = 'prices';
+        $this->cur_link = 'price';
         $this->sidebar_top = $model->sidebar_top;
         $this->sidebar_top_title = $model->sidebar_top_title;
         $this->setMetaTags($model);
@@ -73,6 +74,8 @@ class CategoryController extends BaseController
         $this->cur_chapter = $model->alias;
         $this->sidebar_top = $model->sidebar_top;
         $this->sidebar_top_title = $model->sidebar_top_title;
+
+        $this->cur_link = 'service';
 
         $this->render('view', array(
             'model' => $category
--- www/protected/modules/content/portlets/views/HeaderMenu.php
+++ www/protected/modules/content/portlets/views/HeaderMenu.php
@@ -5,6 +5,7 @@ $data = array(
     "reviews"    => "/reviews",
     "photo"      => "/photo",
     "price"      => "/prices",
+    "service"   => "/service",
     "contacts"   => "/page/contacts"
 );
 ?>
@@ -16,6 +17,8 @@ $data = array(
         {
             $cur_link = Yii::app()->controller->cur_link ? Yii::app()->controller->cur_link : parse_url(Yii::app()->request->getRequestUri(), PHP_URL_PATH);
             $url = strpos($link->url, '/') !== 0 ? '/'.$link->url : $link->url;
+            $cmp_url = isset($data[$cur_link]) ? $data[$cur_link] : $cur_link;
+            $class    = $cmp_url == $url ? ' active' : ' no_active';
             echo CHtml::tag('li', array('class'=> $class), CHtml::link($link->title, $url));
             if (count($links) != ++$i)
             {
